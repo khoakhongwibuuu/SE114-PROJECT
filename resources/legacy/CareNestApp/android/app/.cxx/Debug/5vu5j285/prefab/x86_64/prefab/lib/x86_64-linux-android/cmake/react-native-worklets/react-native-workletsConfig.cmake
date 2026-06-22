9413e0ed45ad8158837d52a9a0ec1302a0e656b7
Tuấn Kiệt <doletuankiet06@gmail.com>
2026-06-23 00:51:20 +0700

    chore(qa): sync QA demo account passwords and archive legacy code

---
+++ resources/legacy/CareNestApp/android/app/.cxx/Debug/5vu5j285/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/react-native-worklets/react-native-workletsConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-worklets::worklets)
+add_library(react-native-worklets::worklets SHARED IMPORTED)
+set_target_properties(react-native-worklets::worklets PROPERTIES
+    IMPORTED_LOCATION "D:/DoAn_MB1/CareNest/frontend/CareNestApp/node_modules/react-native-worklets/android/build/intermediates/cxx/Debug/5l5t46r2/obj/x86_64/libworklets.so"
+    INTERFACE_INCLUDE_DIRECTORIES "D:/DoAn_MB1/CareNest/frontend/CareNestApp/node_modules/react-native-worklets/android/build/prefab-headers/worklets"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
