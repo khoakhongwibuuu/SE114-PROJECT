41233858b895adccd55e5c2e2951151343915520
Tuấn Kiệt <doletuankiet06@gmail.com>
2026-06-29 05:43:18 +0700

    chore: remove legacy React Native code to clean up repo size and language stats

--- resources/legacy/CareNestApp/android/app/.cxx/Debug/5vu5j285/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/react-native-worklets/react-native-workletsConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-worklets::worklets)
-add_library(react-native-worklets::worklets SHARED IMPORTED)
-set_target_properties(react-native-worklets::worklets PROPERTIES
-    IMPORTED_LOCATION "D:/DoAn_MB1/CareNest/frontend/CareNestApp/node_modules/react-native-worklets/android/build/intermediates/cxx/Debug/5l5t46r2/obj/armeabi-v7a/libworklets.so"
-    INTERFACE_INCLUDE_DIRECTORIES "D:/DoAn_MB1/CareNest/frontend/CareNestApp/node_modules/react-native-worklets/android/build/prefab-headers/worklets"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- resources/legacy/CareNestApp/android/app/.cxx/Debug/5vu5j285/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/react-native-nitro-modules/react-native-nitro-modulesConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-nitro-modules::NitroModules)
-add_library(react-native-nitro-modules::NitroModules SHARED IMPORTED)
-set_target_properties(react-native-nitro-modules::NitroModules PROPERTIES
-    IMPORTED_LOCATION "D:/DoAn_MB1/CareNest/frontend/CareNestApp/node_modules/react-native-nitro-modules/android/build/intermediates/cxx/Debug/1t652y4h/obj/x86_64/libNitroModules.so"
-    INTERFACE_INCLUDE_DIRECTORIES "D:/DoAn_MB1/CareNest/frontend/CareNestApp/node_modules/react-native-nitro-modules/android/build/headers/nitromodules"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
